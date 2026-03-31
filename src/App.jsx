46be3d43b4c604d94591c0b537d1e12fdfe76691
Rakshita-git-art <rakshitasb120@gmail.com>
2026-03-31 13:56:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import History from "./pages/History";
 import TrainingData from "./pages/TrainingData";
-import TrainingLog from "./pages/TrainingLog";
+
 import ReportsPage from "./pages/ReportsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+      <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/history" element={<History />} />
           <Route path="/training" element={<TrainingData />} />
           <Route path="/logbook/:sessionId?" element={<TrainingLog />} />
-          <Route path="/reports/:sessionId" element={<ReportsPage />} />
+          <Route path="/reports/:sessionId?" element={<Reports />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
