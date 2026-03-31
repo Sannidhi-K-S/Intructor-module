17c666745189376fe0cfe294b3274a02bc4c0e5c
Rakshita-git-art <rakshitasb120@gmail.com>
2026-03-31 12:08:07 +0530

    Update App.jsx

--- Downloads/Intructor-module-main/Intructor-module-main/src/App.jsx
+++ Downloads/Intructor-module-main/Intructor-module-main/src/App.jsx
@@ -3,7 +3,7 @@ import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import History from "./pages/History";
 import TrainingData from "./pages/TrainingData";
-import TrainingLog from "./pages/TrainingLog";
+import Reports from "./pages/Reports";
 
 function App() {
   return (
@@ -14,8 +14,10 @@ function App() {
           <Route path="/history" element={<History />} />
           <Route path="/training" element={<TrainingData />} />
           <Route path="/logbook/:sessionId?" element={<TrainingLog />} />
+          <Route path="/reports/:sessionId?" element={<Reports />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
+       
       </Routes>
     </BrowserRouter>
   );
